4a29371cab2e767209d8d27278feb54da2cfa513
Sam Arch <sam.arche912912@gmail.com>
2026-06-01 10:36:58 -0500

    Add APACHE license and sort headers

--- src/adbc_insert.cpp
+++ src/adbc_insert.cpp
@@ -1,9 +1,26 @@
-#include "duckdb/main/client_context.hpp"
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #include "adbc_insert.hpp"
 #include "adbc_util.hpp"
+#include "duckdb/common/mutex.hpp"
+#include "duckdb/main/client_context.hpp"
 #include "duckdb/storage/temporary_memory_manager.hpp"
 #include <condition_variable>
-#include "duckdb/common/mutex.hpp"
 
 // Check if an ADBC command went wrong, if it did then save the state and return
 #define CHECKPOINT_STATUS(EXPR)                                                                                        \
--- src/include/adbc_transaction.hpp
+++ src/include/adbc_transaction.hpp
@@ -1,11 +1,28 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #pragma once
 
 #include "adbc_catalog.hpp"
+#include "duckdb/common/mutex.hpp"
 #include "duckdb/original/std/memory.hpp"
 #include "duckdb/storage/storage_extension.hpp"
 #include "duckdb/transaction/transaction.hpp"
 #include "duckdb/transaction/transaction_manager.hpp"
-#include "duckdb/common/mutex.hpp"
 
 namespace duckdb {
 namespace adbc {
